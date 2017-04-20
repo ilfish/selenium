9e3bbe2fd0a03d3fce83fcb72c1f47117a672194
ylb01 <2267915365@qq.com>
2017-04-20 14:15:40 +0800

    Initial commit

---
+++ all_test.py
@@ -0,0 +1,13 @@
+import unittest
+import test_login2
+
+suite=unittest.TestSuite()
+
+suite.addTest(test_login2.TestLogin("test_null"))
+suite.addTest(test_login2.TestLogin("pawd_null"))
+suite.addTest(test_login2.TestLogin("user_null"))
+suite.addTest(test_login2.TestLogin("error"))
+
+if __name__=='__main__':
+    runner=unittest.TextTestRunner()
+    runner.run(suite)
